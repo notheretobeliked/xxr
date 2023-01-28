f6164eb5659acf4bacde843fb1b17ebed587f58b
Erik Hartin <erik@sans-titre.net>
2023-01-28 19:23:25 +0000

    initial commit

---
+++ src/lib/components/Word.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let word: string;
+	export let image: string;
+
+</script>
+
+
+	<figure>
+		<picture>
+			<source media="(min-width: 50px)"  srcset="/words/{image}.webp" type="image/webp"  />
+			<img src="/words/{image}.png" alt={word} />
+		</picture>
+	</figure>
+	<span class="sr-only">{word}</span>
+
+<style>
+  figure {
+    height: 100px;
+  }
+  img {
+    height: 100%;
+  }
+</style>
