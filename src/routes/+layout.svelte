746f1ca855ecae9e73bb499771b8984ea5d74b23
Erik Hartin <erik@sans-titre.net>
2023-02-07 22:48:27 +0000

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,30 @@
 <script>
-  import "../app.css";
-  import { Modals, closeModal } from 'svelte-modals'
+	import '../app.css'
+	import { Modals, closeModal } from 'svelte-modals'
+	import { dev } from '$app/environment'
+	import { inject } from '@vercel/analytics'
+
+	inject({ mode: dev ? 'development' : 'production' })
 </script>
 
 <slot />
 <Modals>
-  <div
-    slot="backdrop"
-    class="backdrop"
-    on:click={closeModal}
-    on:keypress={closeModal}
-  >
-</div>
+	<div slot="backdrop" class="backdrop" on:click={closeModal} on:keypress={closeModal} />
 </Modals>
 
 <style>
-  .backdrop {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    background: rgba(0,0,0,0.50)
-  }
+	.backdrop {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		background: rgba(0, 0, 0, 0.5);
+	}
 
-  video {
-    height: 100vh;
-    width: 100vw;
-    object-fit: cover;
-  }
+	video {
+		height: 100vh;
+		width: 100vw;
+		object-fit: cover;
+	}
 </style>
