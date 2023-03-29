43a2963ce4990479fe4754df0120a8f23bc63512
Erik Hartin <erik@sans-titre.net>
2023-03-29 22:46:29 +0100

    Fix og tags

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,7 +4,7 @@ import type { PageLoad } from './$types'
 export const load: PageLoad = () => {
   return {
     url: PUBLIC_URL,
-    pageTitle: 'Surround Parliament on the 21st April 2023',
+    pageTitle: 'Surround Parliament from 21 April 2023',
     metadescription: 'Supporters of Greenpeace, trade unions, RSPB, National Trust and common sense will be gathering in London to demand action on the climate crisis. Will you come? Take our quiz!',
   }
 }
--- src/routes/about/+page.ts
+++ src/routes/about/+page.ts
@@ -4,7 +4,7 @@ import type { PageLoad } from '../$types'
 export const load: PageLoad = () => {
   return {
     url: PUBLIC_URL,
-    pageTitle: 'Surround Parliament on the 21st April 2023: About',
+    pageTitle: 'Surround Parliament from 21 April 2023: About',
     metadescription: 'Who made this website and why?',
   }
 }
